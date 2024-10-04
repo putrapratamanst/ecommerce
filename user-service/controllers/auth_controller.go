bc880814711d59678764f8c7e118f023c12ca6b2
Putra Pratama Nst <putrapratamanst@gmail.com>
2024-10-04 11:37:14 +0700

    feat: improve response api

--- user-service/controllers/auth_controller.go
+++ user-service/controllers/auth_controller.go
@@ -1,47 +1,61 @@
 package controllers
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/putrapratamanst/ecommerce/user-service/models"
 	"github.com/putrapratamanst/ecommerce/user-service/services"
+	"github.com/putrapratamanst/ecommerce/user-service/utils"
 )
 
 type AuthController struct {
 	AuthService services.AuthService
+	validate    *validator.Validate
 }
 
 func NewAuthController(authService services.AuthService) *AuthController {
-	return &AuthController{AuthService: authService}
+	return &AuthController{
+		AuthService: authService,
+		validate:    validator.New(),
+	}
 }
 
 func (ctrl *AuthController) Login(c *fiber.Ctx) error {
 	var loginData struct {
-		EmailOrPhone string `json:"email_or_phone"`
-		Password     string `json:"password"`
+		EmailOrPhone string `json:"email_or_phone" validate:"required"`
+		Password     string `json:"password" validate:"required"`
 	}
 
 	if err := c.BodyParser(&loginData); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+        return utils.SendResponse(c, fiber.StatusBadRequest, "Invalid input", nil)
+	}
+
+	if err := ctrl.validate.Struct(loginData); err != nil {
+        return utils.SendResponse(c, fiber.StatusBadRequest, "Invalid input validation", nil)
 	}
 
 	token, err := ctrl.AuthService.Login(loginData.EmailOrPhone, loginData.Password)
 	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": err.Error()})
+        return utils.SendResponse(c, fiber.StatusUnauthorized, err.Error(), nil)
 	}
 
-	return c.JSON(fiber.Map{"token": token})
+    return utils.SendResponse(c, fiber.StatusOK, "Login successful", fiber.Map{"token": token})
 }
 
 func (ctrl *AuthController) Register(c *fiber.Ctx) error {
-    var user models.User
+	var user models.User
 
-    if err := c.BodyParser(&user); err != nil {
-        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
-    }
+	if err := c.BodyParser(&user); err != nil {
+        return utils.SendResponse(c, fiber.StatusBadRequest, "Invalid input", nil)
+	}
 
-    if err := ctrl.AuthService.Register(&user); err != nil {
-        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-    }
+	if err := ctrl.validate.Struct(user); err != nil {
+        return utils.SendResponse(c, fiber.StatusBadRequest, "Invalid input validation: " + err.Error(), nil)
+	}
 
-    return c.Status(fiber.StatusCreated).JSON(user)
+	if err := ctrl.AuthService.Register(&user); err != nil {
+        return utils.SendResponse(c, fiber.StatusBadRequest, err.Error(), nil)
+	}
+
+    return utils.SendResponse(c, fiber.StatusCreated, "User registered successfully", user)
 }
--- user-service/models/user.go
+++ user-service/models/user.go
@@ -1,13 +1,13 @@
 package models
 
 import (
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 type User struct {
     gorm.Model
     Name     string `json:"name"`
-    Email    string `json:"email" gorm:"unique"`
-    Phone    string `json:"phone" gorm:"unique"`
-    Password string `json:"-"`
+    Email    string `json:"email" gorm:"unique" validate:"required,email"`
+    Phone    string `json:"phone" gorm:"unique" validate:"required,numeric,max=12"`
+    Password string `json:"password" validate:"required,min=6"`
 }
